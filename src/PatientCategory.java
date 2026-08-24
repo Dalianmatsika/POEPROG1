1e32e865bf6e10a3f2fa7d00196ce24e9307996e
Student <ST10492347@rcconnect.edu.za>
2026-08-24 14:29:11 +0200

    patientcategory

--- src/PatientCategory.java
+++ src/PatientCategory.java
@@ -1,12 +1,5 @@
 public enum PatientCategory {
-    INPATIENT, OUTPATIENT, EMERGENCY;
-
-    public static PatientCategory fromString(String category) {
-        for (PatientCategory c : PatientCategory.values()) {
-            if (c.name().equalsIgnoreCase(category)) {
-                return c;
-            }
-        }
-        throw new IllegalArgumentException("Invalid category: " + category);
-    }
+    INPATIENT,
+    OUTPATIENT,
+    EMERGENCY
 }
